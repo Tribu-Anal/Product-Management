d2e6379d28cc2bd57cbc880789ffed7f2207bb2d
wupa9 <wupaz9@gmail.com>
2016-02-07 04:44:26 -0600

    Reconfig service to allow arrays in POST requests

--- js/services.js
+++ js/services.js
@@ -1,5 +1,10 @@
 var app = angular.module('ProductsApp');
 
+//Temporary url
+var url = 'http://localhost:8080/api';
+
 app.factory('Product', ($resource) => {
-    return $resource('http://localhost:8080/api'); //Temporary url
+    return $resource(url, {} , {
+        save: { method: "POST", isArray: true }
+    });
 });

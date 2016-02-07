266e5089dcb450bcf195adc98324c2e576216521
Alejandro Ferrera <aleferrera@hotmail.com>
2016-02-07 10:11:55 -0600

    Controller synced with index view

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,11 @@ var app = angular.module('ProductsApp');
 app.controller('editmodeController', function($scope) {
     var ctrl = this;
     
-    $scope.products = [];
+    $scope.products = 
+        [
+            {id: 1, name: "Nintendo Wii U"},
+            {id: 2, name: "PS4"}
+        ];
     
     $scope.insert = function(name) {
         $scope.products.push({name: name});
@@ -15,4 +19,5 @@ app.controller('editmodeController', function($scope) {
     
     ctrl.save = function(){
         $scope.$parent.main.data.visible=false;
+}
 });

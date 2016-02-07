2c914c28bb52625c53810545a0b59c8540f96a00
wupa9 <wupaz9@gmail.com>
2016-02-07 06:32:26 -0600

    Gerate id function and insert moved to GET callback

--- js/controllers.js
+++ js/controllers.js
@@ -11,16 +11,27 @@ app.controller('editmodeController', ['$scope', function($scope) {
     
 }]);
 
-app.controller('ProductCtrl', [ '$scope', 'Product',  ($scope, Product) => {
-    $scope.products = Product.query();
+app.controller('ProductCtrl', [ '$scope', 'Product', '$http',  ($scope, Product, $http) => {
+    $scope.products = Product.query( () => {
+        
+        let genId = () => {
+          let pIds = [];
+            
+          for (var i = 0; i < $scope.products.length; i++)  
+              pIds.push($scope.products[i].id);
+            
+          return Math.max.apply(null, pIds)+1;
+        };
+        
+        $scope.insert = function(name) {
+            $scope.products.push( { id: $scope.products.length > 0 ? genId() : 1, name: name } );
+        };
+    });
+    
     $scope.showPrompt = false;
     
     $scope.saveProducts = () => {
         Product.save($scope.products);
-    };
-
-    $scope.insert = function(name) {
-        $scope.products.push({name: name});
     };
 
     $scope.delete = function(index) {

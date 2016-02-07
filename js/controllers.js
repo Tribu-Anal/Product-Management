6e77c71a3c4d04d9e661af2b726f764f07bf2d4d
wupa9 <wupaz9@gmail.com>
2016-02-07 03:14:09 -0600

    Add Product Controller

--- js/controllers.js
+++ js/controllers.js
@@ -9,3 +9,14 @@ app.controller('editmodeController', ['$scope', function($scope) {
     
 }]);
 
+app.controller('ProductCtrl', function($scope) {
+    $scope.products = [];
+
+    $scope.insert = function(name) {
+        $scope.products.push({name: name});
+    };
+
+    $scope.delete = function(index) {
+        $scope.products.splice(index, 1);  
+    };
+});

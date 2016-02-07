6d2f359f74763eb0219e9a5942b97163f82a9d3d
wupa9 <wupaz9@gmail.com>
2016-02-07 07:33:35 -0600

    Code cleanup

--- js/directives.js
+++ js/directives.js
@@ -1,13 +1,12 @@
-(function() {
-    var app = angular.module('ProductsApp');
-    app.directive('inplaceEditing', function() {
-        return {
-                restrict: 'E',
-                scope: {
-                    edit:'='
-                },
-                controller : 'inplaceEditing as value',
-                template: "<div ng-show='edit.visible'> <input type='text'  ng-model='edit.name' ng-model-options='{ getterSetter: true }' autofocus> <input type='button' ng-click='value.save()' value='Save'></div>"
-            }
-    });
-})();
+var app = angular.module('ProductsApp');
+    
+app.directive('inplaceEditing', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            edit:'='
+        },
+        controller : 'inplaceEditing as value',
+        template: "<div ng-show='edit.visible'> <input type='text'  ng-model='edit.name' ng-model-options='{ getterSetter: true }' autofocus> <input type='button' ng-click='value.save()' value='Save'></div>"
+    }
+});

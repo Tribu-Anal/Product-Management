d155497d7bd8748c568f291f348a4c37349a3291
Daniel Perez <danielperez@unitec.edu>
2016-02-07 20:18:23 -0600

    fix style

--- js/directives.js
+++ js/directives.js
@@ -7,7 +7,7 @@ app.directive('inplaceEditing', function() {
             edit:'='
         },
         controller : 'editmodeController as editCtrl',
-        template: "<div ng-show='edit.editMode'> <input type='text' id='{{edit.id}}'  ng-model='edit.name' ng-model-options='{ getterSetter: true }' ng-keypress='editCtrl.enterEvent($event)'></div>"
+        template: "<div class='col-xs-10 list-group-item' ng-show='edit.editMode'> <input type='text' id='{{edit.id}}'  ng-model='edit.name' ng-model-options='{ getterSetter: true }' ng-keypress='editCtrl.enterEvent($event)'></div>"
         
     }
 });
